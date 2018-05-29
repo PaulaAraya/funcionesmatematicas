bc62a596a6ffbafc58996b3e0f93247ca50be928
PaulaAraya <paula.arayawilliams@gmail.com>
2018-05-29 02:14:47 -0300

    operaciones matematicas funcionan

---
+++ app.js
@@ -0,0 +1,20 @@
+//crear variables con numeros
+//hacer funcion con add y multiply
+//hacer variable con ecuacion
+//imprimir resultado final en alert
+
+let a = 777
+let b = 9824 
+let c = 36325
+
+function add(o,p){
+  return o + p;
+}
+ 
+function multiply(o,p){
+  return o * p;
+}
+
+let ecuation = multiply(c, add(a,b));
+
+alert("es " + ecuation)
